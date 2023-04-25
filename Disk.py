cb461220eaf459c2146024f13f3940536196ba8d
amirhossein-mlkz <amirhossein.mlkz@gmail.com>
2023-04-25 18:42:40 +0430

    v=0.0.1

--- Disk.py
+++ Disk.py
@@ -1,4 +1,12 @@
-
+#--------------------------------
+# Powerd by: AmirHossein Malekzadeh
+# start in: 1402/02/03
+# Company: Dorsa-co
+# Website: https://Dorsa-co.ir
+#
+#
+#
+#--------------------------------
 
 import shutil
 import numpy as np
@@ -6,8 +14,15 @@ import os
 from datetime import datetime
 from datetime import timedelta
 
+
+
 #____________________________________________________________________________________________________________________________________
 #
+#                                       This Class is for store memory space
+# method: toGB():
+#   return space in GB
+# method: toPercent():
+#   return space ercent based on all space
 #
 #____________________________________________________________________________________________________________________________________
 class Space:
@@ -24,7 +39,7 @@ class Space:
         return self.byte /1e3
     
     def toPercent(self):
-        return round(self.byte/self.total_byte, 3)*100
+        return round(self.byte/self.total_byte, 3) * 100
 
 #____________________________________________________________________________________________________________________________________
 #
@@ -73,26 +88,30 @@ class fileParms:
                 self._file_name = self._full_name
 
     #---------------------------------------------------------------------
-    def __dir_size__(self, path):
+    def __calc_dir_size__(self, path):
         total = 0
         with os.scandir(path) as it:
             for entry in it:
                 if entry.is_file():
                     total += entry.stat().st_size
                 elif entry.is_dir():
-                    total += self.__dir_size__(entry.path)
+                    total += self.__calc_dir_size__(entry.path)
         return total
+    
     #---------------------------------------------------------------------
     def path(self):
         return self._path
 
+
     def name(self):
         self.__extract_names_info__()
         return self._file_name
-    
+
+
     def extention(self):
         self.__extract_names_info__()
         return self._file_extention
+
 
     def full_name(self):
         self.__extract_names_info__()
@@ -114,7 +133,7 @@ class fileParms:
     def size(self) -> Space:
         self.__extract_names_info__()
         if self._file_extention == '':
-            self._size = Space(self.__dir_size__(self._path))
+            self._size = Space(self.__calc_dir_size__(self._path))
         else:
             self._size = Space(os.path.getsize( self._path ))
         return self._size
@@ -125,8 +144,6 @@ class fileParms:
         if self._lifetime is None:
             self._lifetime = datetime.today() - self.creation_date()
         return self._lifetime
-    
-
     
 
 #____________________________________________________________________________________________________________________________________
@@ -155,6 +172,11 @@ class FileAction:
             else:
                 os.remove(file.parms.path())
         return _delete_
+    
+    #copy_and_shortcud
+    #rename
+    #shortuc
+
 #____________________________________________________________________________________________________________________________________
 #
 #
@@ -169,12 +191,12 @@ class File:
         return os.path.isdir(self.parms.path())
 
 
-
 class FileManager:
     def __init__(self):
         self.file = None
         #self.actions = FileAction()
         self.operations = []
+        self.log = []
 
     def set_file(self, file:File):
         self.file = file
@@ -183,18 +205,16 @@ class FileManager:
         opr = {'condition':condition, 'action':action}
         self.operations.append(opr)
     
-    def run(self):
+    def run(self, log=True):
         assert self.file is not None, 'set a file before run'
         for opr in self.operations:
             if opr['condition'](self.file):
                 opr['action'](self.file)
-
-
-
-
-
-
-
+                
+                if log:
+                    self.log.append([self.file.parms.path(), opr['action'].__name__ ])
+                return True
+        return False
 
 
 
@@ -202,12 +222,9 @@ class FileManager:
 #
 #
 #____________________________________________________________________________________________________________________________________
-
-
-
-
 
 class Conditions:
+    
     @staticmethod
     def bigger( parm , value):
         if parm > value:
@@ -261,7 +278,25 @@ class Conditions:
         if parm not in value:
             return True
         return False
-    
+
+    @staticmethod
+    def __set_condition_func__(cond_type):
+        if isinstance(cond_type, str):
+            return CONDITIONS[cond_type]
+        return cond_type
+
+
+
+CONDITIONS = {
+        '>=': Conditions.bigger_and_equal,
+        '<=': Conditions.samaller_and_equal,
+        '==': Conditions.equal,
+        '!=': Conditions.not_equal,
+        'in': Conditions.contain,
+        'not in': Conditions.not_contain,
+    }
+
+
 
 
 
@@ -270,19 +305,28 @@ class FileCondition:
     @staticmethod
     def __append_calback__( inpt, func):
         if func is not None:
-            return func(inpt)
+            try:
+                return func(inpt)
+            except:
+                print('ERROR: an error occured when {} callback run on {}'.format(func, inpt))
+                return inpt
         return inpt
+    
+    
 
-    @staticmethod
-    def condition(cond_type, value, input_callback=None):
-        def func(inpt):
-            inpt = FileCondition.__append_calback__(inpt, input_callback)
-            return cond_type( inpt , value )
-        return func
+    # @staticmethod
+    # def condition(cond_type, value, input_callback=None):
+    #     cond_type = Conditions.__set_condition_func__(cond_type) #convert str condtion to related function
+    #     def func(inpt):
+    #         inpt = FileCondition.__append_calback__(inpt, input_callback)
+    #         return cond_type( inpt , value )
+    #     return func
 
     #--------------------------------------------------------------
     @staticmethod
     def creation_time(cond_type, dt, input_callback=None):
+        cond_type = Conditions.__set_condition_func__(cond_type) #convert str condtion to related function
+        assert cond_type not in [Conditions.contain, Conditions.not_contain], " 'in' and 'not in' aren't availble for this condition"
         def func(file:File):
             inpt = FileCondition.__append_calback__(file.parms.creation_date(), input_callback)
             return cond_type( inpt , dt )
@@ -290,8 +334,11 @@ class FileCondition:
 
     #--------------------------------------------------------------
     @staticmethod
-    def file_extention_is(cond_type, ex, input_callback = None):
+    def file_extention(cond_type, ex, input_callback = None):
         assert '.' not in ex, 'input extention without (.) char'
+        cond_type = Conditions.__set_condition_func__(cond_type) #convert str condtion to related function
+        assert cond_type in [Conditions.contain, Conditions.not_contain, Conditions.equal, Conditions.not_equal], " only 'in' , 'not in' , '==' and '!=' aren availble for this condition"
+
         def func(file:File):
             inpt = FileCondition.__append_calback__(file.parms.extention(), input_callback)
             return cond_type( inpt , ex )
@@ -300,58 +347,156 @@ class FileCondition:
     #--------------------------------------------------------------
     @staticmethod
     def file_lifetime(cond_type, age:datetime, input_callback = None):
+        cond_type = Conditions.__set_condition_func__(cond_type) #convert str condtion to related function
+        assert cond_type not in [Conditions.contain, Conditions.not_contain], " 'in' and 'not in' aren't availble for this condition"
+
         def func(file:File):
             inpt = FileCondition.__append_calback__(file.parms.lifetime(), input_callback)
             return cond_type( inpt , age )
         return func
 
+    #--------------------------------------------------------------
+    @staticmethod
+    def file_name(cond_type, value, input_callback = None):
+        cond_type = Conditions.__set_condition_func__(cond_type) #convert str condtion to related function
+
+        def func(file:File):
+            inpt = FileCondition.__append_calback__(file.parms.name(), input_callback)
+            return cond_type( inpt , value )
+        return func
 
     #--------------------------------------------------------------
-    def file_name_is(file:File, name):
-        pass
+    @staticmethod
+    def is_file_name_numeric( input_callback = None):
+        def func(file:File):
+            inpt = FileCondition.__append_calback__(file.parms.name(), input_callback)
+            return inpt.isnumeric()
+        return func
+
 
 
 #____________________________________________________________________________________________________________________________________
 #
 #
 #____________________________________________________________________________________________________________________________________
-def condition(file:File):
-        try:
-            if int(file.parms.name()) > 2022:
-                return True
-            return False
-        except:
-            return False
+#def scanner(path, deep=1):
+    #pass
+class Scanner:
+
+    def __init__(self, file_manager: FileManager):
+        self.file_manager = file_manager
+        self.__scan_location_calback_func__ = None
+        self.__scan_file_calback_func__ = None
+
+    def __join_path__(self, fname, path):
+        return os.path.join( path, fname )
+    
+    def set_scan_location_calback(self, func):
+        self.__scan_location_calback_func__ = func
+
+    def set_scan_file_calback(self, func):
+        self.__scan_file_calback_func__ = func
+
+    #----------------------------------------------------------------------
+    #
+    #
+    #----------------------------------------------------------------------
+    def count_items(self, path, deep=1):
+        total = 0
+        if deep >= 0:
+            for fname in os.listdir(path):
+                fpath = self.__join_path__(fname, path)
+                total+=1
+                if  os.path.isdir(fpath):
+                    total += self.count_items(fpath, deep=deep-1)
+
+        return total
+    #----------------------------------------------------------------------
+    #
+    #
+    #----------------------------------------------------------------------
+    def advanced_count_items(self, path, deep=1, condition = lambda x:True):
+        total = 0
+        if deep >= 0:
+            for fname in os.listdir(path):
+                fpath = self.__join_path__(fname, path)
+                file = File(fpath)
+                if condition(file):
+                    print(file.parms.name())
+                    total+=1
+                if  os.path.isdir(fpath):
+                    total += self.advanced_count_items(fpath, deep=deep-1, condition=condition)
+
+        return total
+
+    #----------------------------------------------------------------------
+    #
+    #
+    #----------------------------------------------------------------------
+    def scan(self, path, deep=1):
+        #------------------------ for Callback -----------------------
+        if self.__scan_location_calback_func__:
+            self.__scan_location_calback_func__(path)
+        #-------------------------------------------------------------
+        if deep >= 0:
+            for fname in os.listdir(path):
+                fpath = self.__join_path__(fname, path)
+
+                #------------------------ for Callback -----------------------
+                if self.__scan_file_calback_func__:
+                    self.__scan_file_calback_func__(fpath)
+                #-------------------------------------------------------------
+
+                #print(fpath)
+                file = File(fpath)
+                self.file_manager.set_file(file)
+                flag = self.file_manager.run()
+
+                if (not flag) and file.is_folder():
+                    self.scan(fpath, deep=deep-1)
 
 
-condition
-#disk_size_info = list(map( lambda x:x/1024000000, disk_size_info ))
-dm = diskMemory('c:\\')
+#____________________________________________________________________________________________________________________________________
+#
+#
+#____________________________________________________________________________________________________________________________________
+class myProjectConditions:
+
+    def __init__(self):
+        self.be_folder = FileCondition.file_extention('==', '')
+        self.be_image = FileCondition.file_extention('in', ['jpg', 'png', 'jpeg'])
+        self.name_older = FileCondition.file_name('>=', 2022, int)
+        self.numberic_name = FileCondition.is_file_name_numeric()
+
+    def condition_date_folder(self,file:File):
+            if self.be_folder(file) and self.numberic_name(file) :
+                if self.name_older(file):
+                    return True
+                return False
+            return False
+    
+    def condition_img(self, file:File):
+        if self.be_image(file):
+            return True
+        return False
+    
+
+def my_scan_file_calback(f):
+    print('Scanning:', f)
+
+
+#-------custom for yourself ----------
+my_cond = myProjectConditions()
+
+#dm = diskMemory('c:\\')
 main_path = 'files'
-f = File('files\steve-jobs-black-and-white.jpg')
-#func_life_time = FileCondition.file_lifetime(Conditions.bigger, timedelta(hours=1))
-#func_life_time(f)
-
 fm = FileManager()
-fm.add_operation(condition, FileAction.delete())
-for fname in os.listdir(main_path):
-    path = os.path.join(main_path, fname)
-    my_file = File(path)
-    fm.set_file(my_file)
-    fm.run()
-#fm = File()
+fm.add_operation(my_cond.condition_date_folder, FileAction.delete())
+#----------------
+scanner = Scanner(fm)
+#scanner.set_scan_file_calback(my_scan_file_calback)
+scanner.scan(main_path, deep=3)
 
 
-
-
-print(dm.used.toPercent())
-print(os.path.dirname('ami/123/digi'))
-
-'''
-
-if condtion(file) ->  action(copy):
-
-
-[ [  x1 or x2 ] and [x]  ]
-
-'''
+print( scanner.count_items(main_path, deep=100))
+print( scanner.advanced_count_items(main_path, deep=100, condition=my_cond.be_image) )
